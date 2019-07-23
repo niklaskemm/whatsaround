6e5dc366e11d364f8b82e0217b86a549b6580f17
Hannes Braun <hannesbraun92@gmail.com>
2019-07-23 16:19:00 +0200

    Hannes Bullshit TTS

--- app/src/main/java/lbs/whatsaround/MainActivity.kt
+++ app/src/main/java/lbs/whatsaround/MainActivity.kt
@@ -1,8 +1,11 @@
 package lbs.whatsaround
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import lbs.whatsaround.TTS
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+
+        mInstance = this
+
     }
+
+    // Needed to get Context in TTS Class
+    companion object {
+        lateinit var mInstance: MainActivity
+        fun getContext(): Context? {
+            return mInstance.applicationContext
+        }
+    }
+
 }
 
